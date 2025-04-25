17f82b564cde3b4f106e7ef7e6bd414b29a04aa4
Maksin Spizhuk <indcheat@mail.ru>
2025-04-25 21:51:58 +0300

    added ready toDo list

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const content = document.getElementById("text").value;
+  const todoContent = document.getElementById("todo__list");
+
+  if (content.trim() !== "") {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+
+    li.textContent = content;
+    button.textContent = "Delete";
+
+    button.addEventListener("click", () => {
+      todoContent.removeChild(li);
+    });
+
+    todoContent.appendChild(li);
+    li.appendChild(button);
+
+    document.getElementById("text").value = "";
+  }
+});
+
+document.getElementById("text").addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    document.getElementById("form").dispatchEvent(new Event("submit"));
+  }
+});
